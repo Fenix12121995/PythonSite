1c1863b06e1504b734eefb23dcbd010449bc1351
Fenix12121995 <Fenix12121995@mail.ru>
2021-05-28 02:23:42 +0400

    Lesson_4
    Lesson_4

---
+++ Lesson_4_Rutkevich/my_framework/templator.py
@@ -0,0 +1,16 @@
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+# import os
+from jinja2 import Template
+
+
+def render(template_name, folder='templates', **kwargs):
+    # file_path = os.path.join(folder, template_name)
+    # with open(file_path, encoding='utf-8') as f:
+    #     template = Template(f.read())
+
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
+
+    return template.render(**kwargs)
---
+++ Lesson_4_Rutkevich/urls.py
@@ -0,0 +1,23 @@
+from views import *
+
+
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/err/': Err(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
+}
